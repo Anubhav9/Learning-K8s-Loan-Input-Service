5747c8183c4979e4408c3e0ec245a9a048812371
Anubhav Sanyal <princebest3@rediffmail.com>
2025-01-25 15:57:45 +0530

    Corrected structure of ingress block

--- main.tf
+++ main.tf
@@ -14,27 +14,27 @@ resource "aws_instance" "loan-input-service"{
 resource "aws_security_group" "loan-input-service-security-group"{
     name="Loan Input Service Security Group"
     description="Security Group for Loan Input Service"
-    ingress=[{
+    ingress={
         description="HTTP Ingress Port"
         from_port=80
         to_port=80
         protocol="tcp"
         cidr_blocks=["0.0.0.0/0"]
-    },
-    {
+    }
+    ingress={
         description="HTTPS Ingress Port"
         from_port=443
         to_port=443
         protocol="tcp"
         cidr_blocks=["0.0.0.0/0"]
     },
-    {
+    ingress={
         description="Custom TCP Port to access application"
         from_port=2809
         to_port=2809
         protocol="tcp"
         cidr_blocks=["0.0.0.0/0"]
-    }]
+    }
     egress {
         from_port   = 0
         to_port     = 0
