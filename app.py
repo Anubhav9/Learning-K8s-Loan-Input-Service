c70ffe40b962a1204a17eed02e792c88ef633d77
Anubhav Sanyal <princebest3@rediffmail.com>
2025-01-27 13:56:45 +0530

    creating new tag

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 def loan_info_home_page():
     return render_template("index.html")
 
+@app.route("/submit",methods=["POST"])
+def get_judgement():
+    annual_income=request.form.get("input-annual-income")
+    formatted_body={
+        "annual_income":annual_income
+    }
+    response=requests.post(url="http://judgement-service:3012/decide",json=formatted_body)
 
 if __name__ == "__main__":
     app.run(port=2809, host='0.0.0.0')
